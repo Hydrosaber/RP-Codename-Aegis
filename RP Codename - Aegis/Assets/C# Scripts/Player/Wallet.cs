a2dad70757a3af4ca9a3576c1bf82d728937d298
BuildTools <unconfigured@null.spigotmc.org>
2017-07-06 12:30:26 -0400

    Started progress on first turret

---
+++ RP Codename - Aegis/Assets/C# Scripts/Player/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallet : MonoBehaviour {
+	[System.Serializable]
+	private int startMoney;
+	private int currentAmt;
+	// Use this for initialization
+	void Start () {
+		currentAmt = startMoney;
+	}
+	public int takeMoney (int amount){
+		currentAmt -= amount;
+		return amount;
+	}
+	public void depositMoney(int amount){
+		currentAmt += amount;
+	}
+}
